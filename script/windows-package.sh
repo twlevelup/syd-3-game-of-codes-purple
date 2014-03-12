58736fb4e35f678cf91829e43ab8e1ec4bff8d18
James Gregory <james@jagregory.com>
2014-03-12 22:15:27 +1100

    Quiet windows script

--- script/windows-package.sh
+++ script/windows-package.sh
@@ -11,12 +11,16 @@ fi
 download='love-0.9.0-win64'
 
 # download and unzip the latest love
-curl -L "https://bitbucket.org/rude/love/downloads/$download.zip" > /tmp/$download.zip
-unzip -o /tmp/$download.zip -d /tmp
+echo Downloading $download
+curl -s -L "https://bitbucket.org/rude/love/downloads/$download.zip" > /tmp/$download.zip
+unzip -q -o /tmp/$download.zip -d /tmp
 
 # combine the exe with the lua files
+echo Building levelup.exe
 cat dist/levelup.love /tmp/$download/love.exe > /tmp/$download/levelup.exe
 
 # zip it and move it to dist
-pushd /tmp/$download && zip levelup.zip * -x love.exe && popd
+pushd /tmp/$download > /dev/null && zip -q levelup.zip * -x love.exe && popd > /dev/null
 mv /tmp/$download/levelup.zip dist/windows-levelup.zip
+
+echo Complete
