3a142ee925a976ec10fb519bf4af33ffeee86342
Scott Sidwell <scottiesid@gmail.com>
2014-04-07 16:23:04 +1000

    Added ground sprite and refactored its tests

--- ground.lua
+++ ground.lua
@@ -5,22 +5,29 @@ Ground.__index = Ground
 setmetatable(Ground, {__index = Entity})
 
 function Ground:new(game)
-  local newGround = {
-    x = 0,
-    y = 0.9 * game.window.getHeight(),
-    graphics = love.graphics,
-    size = {
-      x = game.window.getWidth(),
-      y = 0
+    local ground = Entity:new(game)
+    ground.x = 0
+    ground.y = 0.9 * game.window.getHeight()
+    ground.size = {
+            x = game.window.getWidth(),
+            y = 200
     }
-  }
+    ground.graphics = {
+        source = "assets/images/skyline-ground.png"
+    }
 
-  return setmetatable(newGround, self)
+    if game.graphics ~= nil then
+        ground.graphics.image = game.graphics.newImage(ground.graphics.source)
+        ground.graphics.image:setWrap("repeat", "clamp")
+        ground.graphics.quad = game.graphics.newQuad(0, 0, ground.size.x, ground.size.y, 45, 200)
+    end
+    return setmetatable(ground, self)
 end
 
 function Ground:draw()
-  self.graphics.line(self.x, self.y, self.x + self.size.x, self.y + self.size.y)
-  self.graphics.circle("fill", 300, self.y, 3, 100) 
+-- self.game.graphics.line(self.x, self.y, self.x + self.size.x, self.y + self.size.y)
+-- self.game.graphics.circle("fill", 300, self.y, 3, 100)
+    self.game.graphics.draw(self.graphics.image, self.graphics.quad, self.x, self.y)
 end
 
 function Ground:update(dt)
--- timer.lua
+++ timer.lua
@@ -6,44 +6,42 @@ Timer.__index = Timer
 setmetatable(Timer, {__index = Entity})
 
 function Timer:new(game, config)
-  local config = config or {}
+    local config = config or {}
 
-  local newTimer = Entity:new(game)
+    local newTimer = Entity:new(game)
+    newTimer._timeLimit = config.timeLimit or 0
+    newTimer._timeRemaining = -1
+    newTimer._finished = false
+    newTimer.size = config.size or {
+        x = 50,
+        y = 60
+    }
+    newTimer.x = config.x or game.window.getWidth() * 0.9 - newTimer.size.x
+    newTimer.y = config.y or game.window.getHeight() * 0.1
 
-  newTimer.game = game
-  newTimer._timeLimit = config.timeLimit or 0
-  newTimer._timeRemaining = -1
-  newTimer._finished = false
-  newTimer.size = config.size or {
-      x = 50,
-      y = 60
-  }
-  newTimer.x = config.x or game.window.getWidth() * 0.9 - newTimer.size.x
-  newTimer.y = config.y or game.window.getHeight() * 0.1
-
-  return setmetatable(newTimer, self)
+    return setmetatable(newTimer, self)
 end
 
 function Timer:update(currentTime)
-  if(self._finished) then
-    return
-  end
+    if(self._finished) then
+        return
+    end
 
-  if(self._timeRemaining==-1) then
-    self._timeRemaining = self._timeLimit
-  end
-  self._timeRemaining = self._timeRemaining - currentTime
+    if(self._timeRemaining==-1) then
+        self._timeRemaining = self._timeLimit
+    end
+    self._timeRemaining = self._timeRemaining - currentTime
 
-  self._finished = (self:toString() == "00:00" and true or false)
+    self._finished = (self:toString() == "00:00" and true or false)
 end
 
 function Timer:toString()
-  return os.date(GAME_TIME_FORMAT, self._timeRemaining)
+    return os.date(GAME_TIME_FORMAT, self._timeRemaining)
 end   
 
 function Timer:draw()
-  self.game.graphics.rectangle("line", self.x, self.y, 100, 50)
-  local fontHeight = self.game.graphics.getFont():getHeight()
-  self.game.graphics.printf(self:toString(), self.x, self.size.y+fontHeight, self.size.x, "center", 0, 2, 2)
+    self.game.graphics.rectangle("line", self.x, self.y, 100, 50)
+    local fontHeight = self.game.graphics.getFont():getHeight()
+    self.game.graphics.printf(self:toString(), self.x, self.size.y+fontHeight, self.size.x, "center", 0, 2, 2)
 end
 
