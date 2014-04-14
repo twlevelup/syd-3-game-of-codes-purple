eda3c4e3c6f49fe56ec562ada5680a54227cbf4f
Scott Sidwell <scottiesid@gmail.com>
2014-04-14 14:40:10 +1000

    Updated platform for physics

--- platform.lua
+++ platform.lua
@@ -4,24 +4,23 @@ Platform = {}
 Platform.__index = Platform
 setmetatable(Platform, {__index = Entity})
 
-function Platform:new(game)
+function Platform:new(game, config)
     local config = config or {}
-    local newPlatform = Entity:new(game)  
-
-    newPlatform.height = 30
-    newPlatform.width = 100 or config.width
+    local newPlatform = Entity:new(game, config)
+    newPlatform.x = game.window.getWidth()
+    newPlatform.y = game.window.getHeight() * 2 / 3
     newPlatform.size = config.size or {
-        x = 100,
-        y = 100
+        x = 200,
+        y = 25
     }
 
     return setmetatable(newPlatform, self)
 end
 
 function Platform:update(dt)
-
+    self:updatePhysics(dt)
 end
 
 function Platform:draw()
-    self.graphics.rectangle("fill",self.size.x, self.size.y,self.width, self.height)
+    self.game.graphics.rectangle("line", self.x, self.y, self.size.x, self.size.y)
 end
--- spec/platform_spec.lua
+++ spec/platform_spec.lua
@@ -1,60 +1,50 @@
 require 'platform'
+require 'spec/love-mocks'
 
 describe("Platform", function()
 
-	mock_graphics = function()
-		local graphics_spy = {
-			rectangle = spy.new(function() end),
-		}
-		
-		return graphics_spy
-	end
+    mock_graphics = function()
+        local graphics_spy = {
+            rectangle = spy.new(function() end),
+        }
 
-	mock_game = function()
-		local game_spy = {
-			window = {
-				getWidth = function() return 800 end,
-				getHeight = function() return 600 end
-			}
-		}
-
-		return game_spy
-	end
+        return graphics_spy
+    end
 
     describe("#update", function()
         it("should not move on the screen", function()
-            
-            local platform = Platform:new(game)
+
+            local platform = Platform:new(mock_love())
 
             platform.x = 100
             platform.y = 100
-            platform.width = 100
-            platform.height = 30
-            
+            platform.size.x = 100
+            platform.size.y = 30
+
 
             platform:update(1)
 
 
-            assert.are.equal(30, platform.height)
+            assert.are.equal(30, platform.size.y)
             assert.are.equal(100, platform.x)
             assert.are.equal(100, platform.y)
-            assert.are.equal(100, platform.width)
+            assert.are.equal(100, platform.size.x)
 
         end)
     end)
 
     describe("#draw", function()
-    	it ("should draw platform", function()
-    	game = mock_game
-    	platform = Platform:new(game)
-    	platform.graphics = mock_graphics()
-    	platform:draw()
+        it ("should draw platform", function()
+            game = mock_love()
+            game.graphics = mock_graphics()
+            platform = Platform:new(game)
+            platform:draw()
 
-    	assert.spy(platform.graphics.rectangle).was.called_with("fill", 100, 100, 100, 30)
-    	--assert.spy(platform.graphics.rectangle).was.called()
-    	end )
+            --assert.spy(game.graphics.rectangle).was.called_with("fill", 100, 100, 100, 30)
+            assert.spy(game.graphics.rectangle).was.called()
+        end )
     end)
 
 
-  end)
+end)
 
