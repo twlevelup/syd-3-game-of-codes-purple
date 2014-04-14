c13ebfd745913df8b7e87374aee5c0ff5c68c4eb
Carl Suster <carl@contraflo.ws>
2014-04-14 19:44:00 +1000

    Draw pause screen over game

--- pause.lua
+++ pause.lua
@@ -1,9 +1,21 @@
 pause = {}
 
+function pause:enter(prevState)
+  self.prevState = prevState
+end
+
 function pause:draw()
   -- local text = "Paused.\nPress '" .. love.input.mapping['pause'] .. "' to continue"
-  local text = "Paused."
-  love.graphics.print(text, love.window.getWidth() / 2, love.window.getHeight() / 2)
+
+  self.prevState:draw()
+  local text = "PAUSED"
+
+  local oldFont = love.graphics.getFont()
+  local font = love.graphics.newFont(40)
+  local fontHeight = font:getHeight()
+  love.graphics.setFont(font)
+  love.graphics.print(text, love.window.getWidth() / 2 - 50, love.window.getHeight() / 2 - fontHeight, 0, 1, 1)
+  love.graphics.setFont(oldFont)
 end
 
 function pause:update(dt)
